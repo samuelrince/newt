e5b8e002365be8ca8d5c9a4bd2119d77b8b40b29
Samuel Rince <s@rince.me>
2020-06-22 18:29:57 -0700

    Adding processing.py to process HTML and Text

---
+++ src/processing.py
@@ -0,0 +1,62 @@
+import re
+
+from typing import Union
+from bs4 import BeautifulSoup
+
+
+def preprocess_text(text: str) -> str:
+    """Basic text pre-processing."""
+    text = re.sub(r'(^[\n]+)', '', text)        # Remove \n at the beginning
+    text = re.sub(r'([\n]+$)', '', text)        # Remove \n at the end
+    text = re.sub(r'([\n]{2,})', '', text)      # Remove all double \n
+    text = re.sub('r([\t])', '', text)          # Remove \t
+    text = re.sub(r'([ ]{2,})', ' ', text)      # Remove extra spaces
+    return text
+
+
+def shorter_text(text: str, word_limit: int) -> str:
+    """Makes a text shorter than word_limit."""
+    text_split = text.split()
+    if len(text_split) > word_limit:
+        return ' '.join(text_split[:word_limit]) + '...'
+    else:
+        return text
+
+
+class HTMLProcessing:
+
+    def __init__(self, html: str):
+        self.__soup = BeautifulSoup(html, 'html.parser')
+
+    def get_title(self) -> Union[str, None]:
+        """Extracts the title of a website."""
+        title = None
+
+        if self.__soup.find('h1') is not None:
+            title = self.__soup.find('h1').text
+        elif self.__soup.title is not None:
+            title = self.__soup.title.text
+
+        if title is not None:
+            title = shorter_text(preprocess_text(title), 20)
+
+        return title
+
+    def get_description(self) -> Union[str, None]:
+        """Extracts the description of the article from meta tags."""
+        tmp = self.__soup.findAll('meta', attrs={"name": re.compile(r"description", re.IGNORECASE)})
+        tmp += self.__soup.findAll('meta', attrs={"name": re.compile(r"description", re.IGNORECASE)})
+
+        for desc in tmp:
+            if desc.get('content') is not None:
+                return shorter_text(preprocess_text(desc['content']), 60)
+
+        return None
+
+    def get_content(self) -> Union[str, None]:
+        """Extracts paragraphs from the content of the article."""
+        paragraphs = self.__soup.findAll('p')
+        if paragraphs is not None:
+            return ' '.join(paragraphs)
+
+        return None
